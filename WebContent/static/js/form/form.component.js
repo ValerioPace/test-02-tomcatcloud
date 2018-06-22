53f445f24b1a0a5677b1b8386ae771dfec7d8321
Pace Valerio <v.pace@almaviva.it>
2018-06-22 19:10:33 +0200

    commit form component with static absolute url

--- WebContent/static/js/form/form.component.js
+++ WebContent/static/js/form/form.component.js
@@ -7,12 +7,13 @@ angular.
     //templateUrl: $rootScope.resourceUrl + '/js/form/form.template.html',
     templateUrl: '/noipa-test/resources/js/form/form.template.html',
     controller: 
-      function formController($scope, $rootScope,$http,FORM_CONFIG) {
+      function formController($scope,$http,FORM_CONFIG) {
         $scope.HEADER_CONFIG = FORM_CONFIG;
         $scope.result = [];
         $scope.listEnti = function (){
         	       	
-          $http.get($rootScope.serviceUrl + "/enti/all")
+          $http.get("http://test02-app-test-noipa-cloud.192.168.99.100.nip.io/noipa-test/rest/enti/all")
+          
 	          .then(function(response) {
 	              $scope.result = response;
 	          });
