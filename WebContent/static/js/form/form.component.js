1e1eb1165e8fa0a0c9acd324e600179f26fdd6ff
Pace Valerio <v.pace@almaviva.it>
2018-06-22 18:14:12 +0200

    commit change url relative for template angular

--- WebContent/static/js/form/form.component.js
+++ WebContent/static/js/form/form.component.js
@@ -4,7 +4,7 @@
 angular.
   module('noipa.form').
   component('formComponent', {
-    templateUrl: 'form.template.html',
+    templateUrl: '/resources/js/form/form.template.html',
     controller: 
       function formController($scope, $rootScope,$http,FORM_CONFIG) {
         $scope.HEADER_CONFIG = FORM_CONFIG;
